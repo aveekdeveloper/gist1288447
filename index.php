039cc7d0877247e2bf46a54d4c68a93d0a403131
aveekdeveloper <aveekdeveloper@gmail.com>
2014-05-02 20:28:11 +0530

    directory check

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
+// current directory
+echo getcwd() . "\n";
+
+chdir('ext');
+
+// current directory
+echo getcwd() . "\n";
+
+if ($handle = opendir(getcwd())) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:\n";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "$entry\n";
+    }
+	
+    closedir($handle);
+}
 
 print_r(get_loaded_extensions());
 
